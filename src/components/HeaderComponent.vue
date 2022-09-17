b7276b66d6bda2f49ba6c42d746eb57979392f80
Jonas Petrik <petrik@anwalt.de>
2022-09-17 23:31:45 +0200

    Header /  Particles / Parofile

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,11 +1,8 @@
 <template>
-  <header class="header">
+  <header class="header d-md-none">
     <div
       class="container-fluid-limited d-flex align-items-center justify-content-between"
     >
-      <a href="" class="header-brand">
-        <img src="@/assets/img/logo.svg" alt="Your logo" />
-      </a>
       <button class="sections-nav-toggler" aria-label="Toggle navigation">
         <span class="sections-nav-toggler-bar"></span>
         <span class="sections-nav-toggler-bar"></span>
--- src/components/MainContentComponent.vue
+++ src/components/MainContentComponent.vue
@@ -10,14 +10,14 @@
             <div class="section-body">
               <div class="jumbotron jumbotron-fluid pt-6 pt-lg-8 pb-0 mb-0">
                 <img
-                  src="https://via.placeholder.com/160"
+                  src="@/assets/img/profile-photo.jpg"
                   class="jumbotron-img animation-translate animation-item-1"
                   alt="Avatar"
                 />
                 <h1
                   class="display-1 display-animated display-animated-in animation-translate animation-item-2"
                 >
-                  John<br />Wilson
+                  Jonas<br />Petrik
                 </h1>
                 <p class="lead animation-translate animation-item-3">
                   FREELANCE WEBDEVELOPER
