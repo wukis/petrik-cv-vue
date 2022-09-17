fbb66767e959f4048f07739b6288421621e063ad
Jonas Petrik <petrik@anwalt.de>
2022-09-17 23:57:54 +0200

    Particles added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <template>
+  <vue-particles
+    color="#dedede"
+    :particleOpacity="0.7"
+    :particlesNumber="40"
+    shapeType="circle"
+    :particleSize="4"
+    linesColor="#dedede"
+    :linesWidth="1"
+    :lineLinked="true"
+    :lineOpacity="0.4"
+    :linesDistance="150"
+    :moveSpeed="1"
+    :hoverEffect="true"
+    hoverMode="grab"
+    :clickEffect="false"
+    clickMode="push"
+  >
+  </vue-particles>
   <router-view />
 </template>
 
 <style lang="scss">
 @import "./assets/css/main.scss";
+
+canvas {
+  width: 100%;
+  height: 100%;
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -9999;
+}
 </style>
