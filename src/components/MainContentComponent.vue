4a14c48bb2e7c60f9aafa1894c727bb6db92afb1
Jonas Petrik <petrik@anwalt.de>
2022-09-18 10:02:38 +0200

    Refactor to TopSection

--- src/components/MainContentComponent.vue
+++ src/components/MainContentComponent.vue
@@ -3,35 +3,7 @@
     <div class="container-fluid-limited">
       <div class="row">
         <div class="col col-xl-9">
-          <section
-            id="section-01"
-            class="section section-sub-header animation interaction-in"
-          >
-            <div class="section-body">
-              <div class="jumbotron jumbotron-fluid pt-6 pt-lg-8 pb-0 mb-0">
-                <img
-                  src="@/assets/img/profile-photo.jpg"
-                  class="jumbotron-img animation-translate animation-item-1"
-                  alt="Avatar"
-                />
-                <h1
-                  class="display-1 display-animated display-animated-in animation-translate animation-item-2"
-                >
-                  Jonas<br />Petrik
-                </h1>
-                <p class="lead animation-translate animation-item-3">
-                  FREELANCE WEBDEVELOPER
-                </p>
-              </div>
-            </div>
-            <div class="section-footer animation-translate animation-item-4">
-              <a class="section-next goto-section" href="#section-02">
-                <span class="section-next-counter">01/10</span>
-                <span class="section-next-label">Next chapter</span>
-                <span class="section-next-icon"></span>
-              </a>
-            </div>
-          </section>
+          <TopSection />
           <section id="section-02" class="section animation">
             <div class="section-body">
               <div class="row">
@@ -679,3 +651,15 @@
     </div>
   </main>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TopSection from "@/components/sections/TopComponent.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "MainContentComponent",
+  components: {
+    TopSection,
+  },
+});
+</script>
---
+++ src/components/sections/TopComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <section
+    id="section-01"
+    class="section section-sub-header animation interaction-in"
+  >
+    <div class="section-body">
+      <div class="jumbotron jumbotron-fluid pt-6 pt-lg-8 pb-0 mb-0">
+        <img
+          src="@/assets/img/profile-photo.jpg"
+          class="jumbotron-img animation-translate animation-item-1"
+          alt="Avatar"
+        />
+        <h1
+          class="display-1 display-animated display-animated-in animation-translate animation-item-2"
+        >
+          Jonas<br />Petrik
+        </h1>
+        <p class="lead animation-translate animation-item-3">
+          FREELANCE WEBDEVELOPER
+        </p>
+      </div>
+    </div>
+    <div class="section-footer animation-translate animation-item-4">
+      <a class="section-next goto-section" href="#section-02">
+        <span class="section-next-counter">01/10</span>
+        <span class="section-next-label">Next chapter</span>
+        <span class="section-next-icon"></span>
+      </a>
+    </div>
+  </section>
+</template>
