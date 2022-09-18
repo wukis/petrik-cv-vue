637f015ab7357a5008975f727804abc1fce10e88
Jonas Petrik <petrik@anwalt.de>
2022-09-18 10:06:53 +0200

    Refactor to AboutSection

--- src/components/MainContentComponent.vue
+++ src/components/MainContentComponent.vue
@@ -4,58 +4,8 @@
       <div class="row">
         <div class="col col-xl-9">
           <TopSection />
-          <section id="section-02" class="section animation">
-            <div class="section-body">
-              <div class="row">
-                <div class="col col-xl-10">
-                  <h2
-                    class="section-title animation-translate-overline animation-item-1"
-                  >
-                    About me
-                  </h2>
-                  <article class="article animation-translate animation-item-2">
-                    <p>
-                      I'm freelance webdeveloper lorem ipsum dolor sit amet,
-                      consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                      invidunt ut labore et dolore magna aliquyam erat, sed diam
-                      voluptua. At vero eos et accusam et justo duo dolores et
-                      ea rebum. Stet clita kasd gubergren, no sea takimata
-                      sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor
-                      sit amet, consetetur sadipscing elitr, sed diam nonumy
-                      eirmod tempor invidunt ut labore et dolore magna aliquyam
-                      erat, sed diam voluptua. At vero eos et accusam et justo
-                      duo dolores et ea rebum.
-                    </p>
-                    <p>
-                      Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                      sed diam nonumy eirmod tempor invidunt ut labore et dolore
-                      magna aliquyam erat, sed diam voluptua. At vero eos et
-                      accusam et justo duo dolores et ea rebum. Stet clita kasd
-                      gubergren, no sea takimata sanctus est Lorem ipsum dolor
-                      sit amet. Lorem ipsum dolor sit amet, consetetur
-                      sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-                      ut labore et dolore magna.
-                    </p>
-                    <p>
-                      Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                      sed diam nonumy eirmod tempor invidunt ut labore et dolore
-                      magna aliquyam erat, sed diam voluptua. At vero eos et
-                      accusam et justo duo dolores et ea rebum. Stet clita kasd
-                      gubergren, no sea takimata sanctus est Lorem ipsum dolor
-                      sit amet.
-                    </p>
-                  </article>
-                </div>
-              </div>
-            </div>
-            <div class="section-footer animation-translate animation-item-3">
-              <a class="section-next goto-section" href="#section-03">
-                <span class="section-next-counter">02/10</span>
-                <span class="section-next-label">Next chapter</span>
-                <span class="section-next-icon"></span>
-              </a>
-            </div>
-          </section>
+          <AboutSection />
+
           <section id="section-03" class="section animation">
             <div class="section-body">
               <h2
@@ -655,11 +605,13 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import TopSection from "@/components/sections/TopComponent.vue"; // @ is an alias to /src
+import AboutSection from "@/components/sections/AboutComponent.vue";
 
 export default defineComponent({
   name: "MainContentComponent",
   components: {
     TopSection,
+    AboutSection,
   },
 });
 </script>
