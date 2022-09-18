451994dc799caa4e2b290139953a824929bfdbcc
Jonas Petrik <petrik@anwalt.de>
2022-09-18 10:32:00 +0200

    Adjust top section layout

--- src/components/sections/TopComponent.vue
+++ src/components/sections/TopComponent.vue
@@ -5,19 +5,25 @@
   >
     <div class="section-body">
       <div class="jumbotron jumbotron-fluid pt-6 pt-lg-8 pb-0 mb-0">
-        <img
-          src="@/assets/img/profile-photo.jpg"
-          class="jumbotron-img animation-translate animation-item-1"
-          alt="Avatar"
-        />
-        <h1
-          class="display-1 display-animated display-animated-in animation-translate animation-item-2"
-        >
-          Jonas<br />Petrik
-        </h1>
-        <p class="lead animation-translate animation-item-3">
-          FREELANCE WEBDEVELOPER
-        </p>
+        <div class="row">
+          <div class="col-md-4">
+            <img
+              src="@/assets/img/profile-photo.jpg"
+              class="jumbotron-img animation-translate animation-item-1"
+              alt="Avatar"
+            />
+          </div>
+          <div class="col-md-8">
+            <h1
+              class="display-1 display-animated display-animated-in animation-translate animation-item-2"
+            >
+              Jonas<br />Petrik
+            </h1>
+            <p class="lead animation-translate animation-item-3">
+              FREELANCE SENIOR FULLSTACK DEVELOPER
+            </p>
+          </div>
+        </div>
       </div>
     </div>
     <div class="section-footer animation-translate animation-item-4">
