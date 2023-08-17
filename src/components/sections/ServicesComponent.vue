e2b88b606560841a68cb8fc9726895366cbf2772
Jonas Petrik <petrik@anwalt.de>
2023-08-17 23:09:19 +0200

    Services filled in

--- src/components/sections/ServicesComponent.vue
+++ src/components/sections/ServicesComponent.vue
@@ -17,13 +17,18 @@ import SectionComponent from "./SectionComponent.vue";
               <div class="card card">
                 <div class="card-body">
                   <strong class="card-counter">1</strong>
-                  <h3 class="card-title">Web<br />development</h3>
+                  <h3 class="card-title">Full Stack<br />Web development</h3>
                   <p class="card-text">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                    diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                    aliquyam erat, sed diam voluptua. At vero eos et accusam et
-                    justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-                    sea takimata sanctus est.
+                    Harnessing the synergy of PHP and JavaScript, I provide
+                    comprehensive full stack web development solutions that
+                    cater to modern online demands. With PHP powering the
+                    server-side processes and JavaScript enriching the
+                    client-side interactivity, my approach ensures responsive,
+                    efficient, and dynamic web applications. From intricate
+                    back-end functionalities to seamless front-end experiences,
+                    I encapsulate every layer of web development to deliver a
+                    holistic digital product that resonates with user
+                    expectations and business goals.
                   </p>
                 </div>
               </div>
@@ -32,12 +37,19 @@ import SectionComponent from "./SectionComponent.vue";
               <div class="card card">
                 <div class="card-body">
                   <strong class="card-counter">2</strong>
-                  <h3 class="card-title">Web<br />design</h3>
+                  <h3 class="card-title">Consulting</h3>
                   <p class="card-text">
-                    Stet clita kasd gubergren, no sea takimata sanctus est.
-                    Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-                    consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                    invidunt ut labore et dolore magna aliquyam erat, sed diam.
+                    In the ever-evolving landscape of technology, navigating the
+                    intricacies of development can be daunting. I offer
+                    strategic consulting services tailored to streamline your
+                    development processes. Leveraging a deep understanding of
+                    best practices, methodologies, and emerging trends, I guide
+                    teams and businesses in optimizing their workflows, ensuring
+                    projects are executed efficiently and effectively. Whether
+                    it's selecting the right technology stack, refining agile
+                    practices, or enhancing team collaboration, my insights aim
+                    to position your projects for success from inception to
+                    deployment.
                   </p>
                 </div>
               </div>
@@ -46,13 +58,20 @@ import SectionComponent from "./SectionComponent.vue";
               <div class="card card">
                 <div class="card-body">
                   <strong class="card-counter">3</strong>
-                  <h3 class="card-title">Web<br />consulting</h3>
+                  <h3 class="card-title">Auditing</h3>
                   <p class="card-text">
-                    At vero eos et accusam et justo duo dolores et ea rebum.
-                    Stet clita kasd gubergren, no sea takimata sanctus est.
-                    Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-                    consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                    invidunt ut labore et.
+                    In a digital age marked by rapidly changing technologies and
+                    standards, ensuring your platforms and processes remain at
+                    their peak is paramount. My auditing services dive deep into
+                    the intricacies of your systems, identifying potential
+                    bottlenecks, security vulnerabilities, and areas for
+                    improvement. With a meticulous approach, I evaluate the
+                    efficiency and effectiveness of your digital assets,
+                    ensuring compliance with industry standards and best
+                    practices. From code quality assessments to performance
+                    evaluations, my goal is to provide actionable insights that
+                    bolster your operations, ensuring your digital endeavors are
+                    both robust and future-ready.
                   </p>
                 </div>
               </div>
