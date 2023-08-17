f6ae17fce863e810aba369429f93dd33f0ceedd4
Jonas Petrik <petrik@anwalt.de>
2023-08-17 23:03:57 +0200

    Make navigation interactible

--- src/components/NavigationComponent.vue
+++ src/components/NavigationComponent.vue
@@ -1,67 +1,19 @@
 <template>
   <nav class="sections-nav-container">
     <ul id="sections-nav" class="nav sections-nav sections-nav-animated">
-      <li class="sections-nav-item">
+      <li
+        class="sections-nav-item"
+        v-for="section in sections"
+        :key="section.id"
+      >
         <a
-          href="#section-01"
-          class="nav-link sections-nav-link goto-section active"
+          :href="section.href"
+          class="nav-link sections-nav-link goto-section"
+          :class="{ active: activeSection === section.id }"
+          @click="setActiveSection(section.id)"
         >
-          <span class="sections-nav-counter">01</span>
-          Top
-        </a>
-      </li>
-      <li class="sections-nav-item">
-        <a href="#section-02" class="nav-link sections-nav-link goto-section">
-          <span class="sections-nav-counter">02</span>
-          About me
-        </a>
-      </li>
-      <li class="sections-nav-item">
-        <a href="#section-03" class="nav-link sections-nav-link goto-section">
-          <span class="sections-nav-counter">03</span>
-          My services
-        </a>
-      </li>
-      <li class="sections-nav-item">
-        <a href="#section-04" class="nav-link sections-nav-link goto-section">
-          <span class="sections-nav-counter">04</span>
-          Skills
-        </a>
-      </li>
-      <li class="sections-nav-item">
-        <a href="#section-05" class="nav-link sections-nav-link goto-section">
-          <span class="sections-nav-counter">05</span>
-          Facts
-        </a>
-      </li>
-      <li class="sections-nav-item">
-        <a href="#section-06" class="nav-link sections-nav-link goto-section">
-          <span class="sections-nav-counter">06</span>
-          Experiences
-        </a>
-      </li>
-      <li class="sections-nav-item">
-        <a href="#section-07" class="nav-link sections-nav-link goto-section">
-          <span class="sections-nav-counter">07</span>
-          References
-        </a>
-      </li>
-      <li class="sections-nav-item">
-        <a href="#section-08" class="nav-link sections-nav-link goto-section">
-          <span class="sections-nav-counter">08</span>
-          Projects
-        </a>
-      </li>
-      <li class="sections-nav-item">
-        <a href="#section-09" class="nav-link sections-nav-link goto-section">
-          <span class="sections-nav-counter">09</span>
-          Testimonials
-        </a>
-      </li>
-      <li class="sections-nav-item">
-        <a href="#section-10" class="nav-link sections-nav-link goto-section">
-          <span class="sections-nav-counter">10</span>
-          Contact
+          <span class="sections-nav-counter">{{ section.counter }}</span>
+          {{ section.name }}
         </a>
       </li>
       <li class="sections-nav-item">
@@ -73,3 +25,70 @@
     </ul>
   </nav>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeSection: "section-01",
+      sections: [
+        { id: "section-01", counter: "01", name: "Top", href: "#section-01" },
+        {
+          id: "section-02",
+          counter: "02",
+          name: "About me",
+          href: "#section-02",
+        },
+        {
+          id: "section-03",
+          counter: "03",
+          name: "My Services",
+          href: "#section-03",
+        },
+        {
+          id: "section-04",
+          counter: "04",
+          name: "Skills",
+          href: "#section-04",
+        },
+        { id: "section-05", counter: "05", name: "Facts", href: "#section-05" },
+        {
+          id: "section-06",
+          counter: "06",
+          name: "Experiences",
+          href: "#section-06",
+        },
+        {
+          id: "section-07",
+          counter: "07",
+          name: "References",
+          href: "#section-07",
+        },
+        {
+          id: "section-08",
+          counter: "08",
+          name: "Projects",
+          href: "#section-08",
+        },
+        {
+          id: "section-09",
+          counter: "09",
+          name: "Testimonials",
+          href: "#section-09",
+        },
+        {
+          id: "section-10",
+          counter: "10",
+          name: "Contact",
+          href: "#section-10",
+        },
+      ],
+    };
+  },
+  methods: {
+    setActiveSection(id) {
+      this.activeSection = id;
+    },
+  },
+};
+</script>
